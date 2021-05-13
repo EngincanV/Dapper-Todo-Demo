5946d441c5133d5c971258857d88493997dea873
EngincanV <enginveske@gmail.com>
2021-05-13 23:41:53 +0300

    Fix event runs once error

--- DapperTodoDemo.Web/wwwroot/js/Todo.js
+++ DapperTodoDemo.Web/wwwroot/js/Todo.js
@@ -1,93 +1,100 @@
-﻿$(function() {
-   var container = $("#todo-container"); 
-   var editForm = $("#todo-edit-modal");
-   var editBtn = $(".todo-edit");
-   var deleteForm = $(".todo-delete");
-   
-   $("#create-todo").click(function() {
-      $("#todo-create-modal").modal("show");
-   });
-   
-   //create
-   $("#create-todo-form").submit(function (e) {
-      e.preventDefault();
-      
-      var arr = $(this).serializeArray();
-      var data = {};
-      $(arr).each(function(index, obj){
-         data[obj.name] = obj.value;
-      });
+﻿$(function () {
+    var Todo = function () {
+        function bindEvents(container) {
+            container.on("click", "#create-todo", function () {
+                $("#todo-create-modal").modal("show");
+            });
 
-      $("#todo-create-modal").modal("hide");
-      $.ajax({
-         type: 'POST',
-         url: `/TodoItem/Create`,
-         data: data,
-         success: function (result) {
-            var $newContainer = $(result);
-            container.replaceWith($newContainer);
-         },
-         error: function (err) {
-            console.error(err.responseJSON.error.message);
-         }
-      });
-   })
+            //create
+            container.on("submit", "#create-todo-form", function (e) {
+                e.preventDefault();
 
-   editBtn.click(function () {
-      $("input[name='id']").val($(this).data("todo-id").toString());
-      $("input[name='title']").val($(this).data("todo-title").toString());
-      $("input[name='description']").val($(this).data("todo-description").toString());
+                var arr = $(this).serializeArray();
+                var data = {};
+                $(arr).each(function (index, obj) {
+                    data[obj.name] = obj.value;
+                });
 
-      if(parseInt($(this).data("todo-status")) === 1) {
-         $("input[name='status']").attr("checked", true);
-      }
-      
-      $("#todo-edit-modal").modal("show");
-   });
+                $("#todo-create-modal").modal("hide");
+                $.ajax({
+                    type: 'POST',
+                    url: `/TodoItem/Create`,
+                    data: data,
+                    success: function (result) {
+                        var $newContainer = $(result);
+                        container.html($newContainer);
+                    },
+                    error: function (err) {
+                        console.error(err.responseJSON.error.message);
+                    }
+                });
+            })
 
-   //edit
-   editForm.submit(function (e) {
-      e.preventDefault();
-      
-      var data = {
-         id: $("input[name='id']").val(),
-         title: $("input[name='title']").val(),
-         description: $("input[name='description']").val(),
-         status: parseInt($("input[name='status']").val())
-      };
-      
-      $("#todo-edit-modal").modal("hide");
+            container.on("click", ".todo-edit", function () {
+                $("input[name='id']").val($(this).data("todo-id").toString());
+                $("input[name='title']").val($(this).data("todo-title").toString());
+                $("input[name='description']").val($(this).data("todo-description").toString());
 
-      $.ajax({
-         type: 'POST',
-         url: `/TodoItem/Update`,
-         data: data,
-         success: function (result) {
-            var $newContainer = $(result);
-            container.replaceWith($newContainer);
-         },
-         error: function (err) {
-            console.error(err.responseJSON.error.message);
-         }
-      });
-   })
+                if (parseInt($(this).data("todo-status")) === 1) {
+                    $("input[name='status']").attr("checked", true);
+                }
 
-   //delete
-   deleteForm.click(function () {
-      var id = $(this).data("todo-id");
+                $("#todo-edit-modal").modal("show");
+            });
 
-      $.ajax({
-         type: 'POST',
-         url: `/TodoItem/Delete`,
-         data: {id: id},
-         success: function (result) {
-            var $newContainer = $(result);
-            console.log($newContainer);
-            container.replaceWith($newContainer);
-         },
-         error: function (err) {
-            console.error(err.responseJSON.error.message);
-         }
-      });
-   });
+            //edit
+            container.on("submit", "#todo-edit-modal", function (e) {
+                e.preventDefault();
+
+                var data = {
+                    id: $("input[name='id']").val(),
+                    title: $("input[name='title']").val(),
+                    description: $("input[name='description']").val(),
+                    status: parseInt($("input[name='status']").val())
+                };
+
+                $("#todo-edit-modal").modal("hide");
+
+                $.ajax({
+                    type: 'POST',
+                    url: `/TodoItem/Update`,
+                    data: data,
+                    success: function (result) {
+                        var $newContainer = $(result);
+                        container.html($newContainer);
+                    },
+                    error: function (err) {
+                        console.error(err.responseJSON.error.message);
+                    }
+                });
+            })
+
+            //delete
+            container.on("click", ".todo-delete", function () {
+                var id = $(this).data("todo-id");
+
+                $.ajax({
+                    type: 'POST',
+                    url: `/TodoItem/Delete`,
+                    data: {id: id},
+                    success: function (result) {
+                        var $newContainer = $(result);
+                        console.log($newContainer);
+                        container.html($newContainer);
+                    },
+                    error: function (err) {
+                        console.error(err.responseJSON.error.message);
+                    }
+                });
+            });
+        }
+
+        return {
+            init: function () {
+                bindEvents($("#todo-container"));
+            }
+        }
+    }();
+
+    Todo.init();
 });
